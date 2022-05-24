5c5e2e71fe19f9238cd5f9e91b3e77462d86699e
bphanbc <102866684+bphanbc@users.noreply.github.com>
2022-05-23 22:45:11 -0700

    Create newtests3.tf

---
+++ modules/datastore/newtests3.tf
@@ -0,0 +1,31 @@
+
+   
+resource "aws_s3_bucket" "tests3-bucket" {
+  bucket        = local.s3_bucket_name
+  acl           = "private"
+  force_destroy = var.force_destroy_s3_bucket
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.s3.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  tags = merge(
+    var.standard_tags,
+    {
+      Metaflow = "true"
+    }
+  )
+}
+
+resource "aws_s3_bucket_public_access_block" "tests3-bucket"{
+  bucket = aws_s3_bucket.this.id
+
+  block_public_policy     = true
+  block_public_acls       = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
